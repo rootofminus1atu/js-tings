52bc8c96c575975cbb3fabd3c278523548cd46ac
RootOfMinus1ATU <s00240122@atu.ie>
2023-09-29 10:57:25 +0100

    lab2

--- Sem3/lab2/script.js
+++ Sem3/lab2/script.js
@@ -1,1 +1,29 @@
-console.log("hi")
+function functionExample() {
+    let x = 3
+    let y = "hello"
+    let z = true
+
+    alert(typeof(x))
+    alert(typeof(y))
+    alert(typeof(z))
+
+    alert(`The contents of x are ${x} y contains ${y} and the variable z holds ${z}`)
+
+    y = 22.723
+    alert(typeof(y))
+    alert(`The variable y now holds ${y}`)
+}
+
+function anotherFunction() {
+    let x = 3
+
+    for (var i = 0; i <= x; i++) {
+        document.write(i);
+    }
+
+    alert(`The value of i is now ${i}`)
+    alert(`The value of x is now ${x}`)
+
+    // with `let`, the variable i dies right after the for loop
+    // to keep it for the duration of the whole function we can use `var`
+}
