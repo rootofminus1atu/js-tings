efe698bbd3ac6a39f856586a529c6a7f7b7ec05f
RootOfMinus1ATU <s00240122@atu.ie>
2023-09-29 10:50:09 +0100

    templating and labs

---
+++ Sem3/lab1/script.js
@@ -0,0 +1,9 @@
+
+function sayHello() {
+    const studentID = "s0000"  // this is a string
+
+    const studentName = "Joe"  // this is a string too
+
+    alert(`Hello my name is ${studentName} and my student ID is ${studentID}`)
+    // alert alerts the user by displaying a pop up in the upper side of the screen
+}
---
+++ Sem3/random-tings/templating/index.js
@@ -0,0 +1,40 @@
+const root = document.getElementById("root")
+
+const myDoc = {
+    header: {
+        h1: "Welcome to main page"
+    },
+    nav: {
+        ul: {
+            li: {
+                p: "page 1"
+            },  // issue: can't have multiple elements like this in 1 obj
+            li: {
+                p: "page 2"
+            }
+        }
+    }
+
+}
+
+function checkIfObj(thing) {
+    return typeof thing === "object" && thing !== null && !Array.isArray(thing)
+}
+
+function render(doc, parent) {
+    for (let [elemName, elemContent] of Object.entries(doc)) {
+
+        if (checkIfObj(elemContent)) {
+            const newElement = document.createElement(elemName)
+            parent.appendChild(newElement)
+            render(elemContent, newElement)
+        } else {
+            const newElem = document.createElement(elemName);
+            newElem.innerText = elemContent;
+            parent.appendChild(newElem)
+        }
+        
+    }
+}
+
+render(myDoc, root)
