86f4919ac6590de0acbf781425338968521eccd1
RootOfMinus1ATU <s00240122@atu.ie>
2023-10-06 18:33:09 +0100

    week3

--- Sem3/week3/task1/script.js
+++ Sem3/week3/task1/script.js
@@ -1,6 +1,6 @@
 // for the min/max process
 
-const process = () => {
+function process() {
     const elems = document.getElementsByClassName("num-box")
     const elemsArr = Array.from(elems)
 
@@ -16,7 +16,7 @@ function getNumsFromInputsArray(elemsArr) {
     const nums = elemsArr
         .map(elem => elem.value)
         .map(num => parseInt(num))
-        .filter(item => !Number.isNaN(item))  // filter out the NaN's
+        .filter(item => !Number.isNaN(item))  // filter out the NaN's (the parse might fail)
 
     return nums
 }
@@ -47,28 +47,28 @@ function appendH2(parent, text) {
 // for generating texboxes
 
 function range(n) {
+    // just a convenience function for cleaner code
     return [...Array(n).keys()]
 }
 
-const generateTextboxes = () => {
+function generateInputBoxes() {
     const boxContainer = document.getElementById("boxes")
 
     const howMany = 3;
 
-    range(howMany).forEach((_) => {
-        const textInputElem = document.createElement("input")
-        textInputElem.type = "text";
-        textInputElem.className = "num-box";
-        textInputElem.placeholder = "some number"
-        
-        boxContainer.appendChild(textInputElem);
+    range(howMany).map((_) => {
+        appendNumBoxInput(boxContainer)
     });
 };
 
+function appendNumBoxInput(parent) {
+    const textInputElem = document.createElement("input")
+    textInputElem.type = "text";
+    textInputElem.className = "num-box";
+    textInputElem.placeholder = "some number"
+    
+    parent.appendChild(textInputElem);
+}
 
 
-
-
-
-
-generateTextboxes();
+generateInputBoxes();
--- Sem3/week3/task2/script.js
+++ Sem3/week3/task2/script.js
@@ -1,35 +1,38 @@
-// take hobbies to TASK 4 instead
 
-function processHobbies() {
-    const checkBoxes = document.getElementsByName("hobby")
+let isDivStyle1 = true;
 
-    const hobbies = Array.from(checkBoxes)
-        .filter(elem => elem.checked)
-        .map(elem => elem.value)
-
-    displayHobbies(hobbies)
-
-}
-
-
-function displayHobbies(hobbies) {
-    const container = document.getElementById("result")
-
-    cleanUp(container)
-
-    hobbies.map(hobby => {
-        appendH2(container, hobby)
-    })
-}
-
-function cleanUp(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
+function divStyleChange() {
+    const divStyle = document.getElementById("the-div").style
+    
+    if (isDivStyle1) {
+        divStyle.animation = "glow-animation 1s linear infinite"
+        divStyle.backgroundImage = 'url("images/crab-rave.gif")'
+        divStyle.fontSize = "4rem"
+        divStyle.display = "flex"
+    } else {
+        divStyle.animation = "none"
+        divStyle.backgroundImage = 'url("images/crab-calm.webp")'
+        divStyle.fontSize = "1rem"
+        divStyle.display = "block"
     }
+
+    isDivStyle1 = !isDivStyle1
 }
 
-function appendH2(parent, text) {
-    const h2Elem = document.createElement("h2")
-    h2Elem.innerHTML = text
-    parent.appendChild(h2Elem)
+let h1IsStyle1 = true;
+
+function h1StyleChange() {
+    const h1Style = document.getElementById("the-h1").style
+
+    if (h1IsStyle1) {
+        h1Style.transform = "rotate(180deg)"
+        h1Style.letterSpacing = "1rem"
+        h1Style.fontFamily = "'Comic Sans MS', cursive"
+    } else {
+        h1Style.transform = "none"
+        h1Style.letterSpacing = ""
+        h1Style.fontFamily = ""
+    }
+
+    h1IsStyle1 = !h1IsStyle1
 }
